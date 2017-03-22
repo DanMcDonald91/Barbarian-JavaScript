fc3bc5d692f547dedd88c09ad23ad7a5a191bff9
Daniel McDonald <danielkg91@gmail.comssh-keygen -t rsa -C danielkg91@gmail.com>
2017-03-22 09:28:16 +0000

    initial tests

--- specs/barbarian_hw_specs.js
+++ specs/barbarian_hw_specs.js
@@ -7,5 +7,20 @@ describe('Barbarian', function(){
     assert.equal("Maximus", barbarian.name);
   });
 
+  it('should have the weapon LongSword' , function(){
+    assert.equal("LongSword" , barbarian.weapon);
+  });
+
+
+});it('should have MadDog as favourite beer', function(){
+  assert.equal("MadDog" , barbarian.favbeer);
 
 });
+
+it('should have 100 starting health', function(){
+  assert.equal(100, barbarian.health);
+})
+
+it('should have 0 anger at first' , function(){
+  assert.equal(0, barbarian.anger);
+})
